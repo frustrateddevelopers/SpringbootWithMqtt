58b87ecb11a57e1d4f942852c66009971a07ed84
frustrateddevelopers <frustrateddevelopers@gmail.com>
2016-01-01 16:28:28 +0530

    removed tmp files

--- src/main/java/com/fd/tmp/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.fd.tmp;
-
-/**
- * Created by qasim on 1/1/16.
- */
-public abstract class Constant {
-    public static String K_SOS = "sos/+";
-    public static String K_TAXI = "driver/+/location";
-}
